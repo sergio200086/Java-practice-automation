84d9212582f893bd50098216a2641ff8c1eb42fa
Sergio Suárez <62405866+sergio200086@users.noreply.github.com>
2026-03-20 17:17:18 -0500

    Updated chrome options and yml

--- src/test/java/utils/DriverManager.java
+++ src/test/java/utils/DriverManager.java
@@ -2,13 +2,18 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
 
     private final WebDriver webDriver;
 
     public DriverManager(String url){
-        this.webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        this.webDriver = new ChromeDriver(options);
         this.webDriver.get(url);
     }
 
